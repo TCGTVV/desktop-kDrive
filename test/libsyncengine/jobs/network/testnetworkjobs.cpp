a7e6bfc20caed6a4587436eee9620714bdfbe1e8
Luc Guyot <luc.guyot@infomaniak.com>
2026-03-26 17:24:14 +0100

    refactor(snapshot-item): Introduce the derived class RemoteSnapshotItem

--- test/libsyncengine/jobs/network/testnetworkjobs.cpp
+++ test/libsyncengine/jobs/network/testnetworkjobs.cpp
@@ -989,7 +989,7 @@ void TestNetworkJobs::testFullFileListWithCursorCsv() {
 
         int counter = 0;
         std::string cursor = job.getCursor();
-        SnapshotItem item;
+        RemoteSnapshotItem item;
         bool error = false;
         bool ignore = false;
         bool eof = false;
@@ -1016,7 +1016,7 @@ void TestNetworkJobs::testFullFileListWithCursorCsvZip() {
         CPPUNIT_ASSERT(exitCode == ExitCode::Ok);
         int counter = 0;
         std::string cursor = job.getCursor();
-        SnapshotItem item;
+        RemoteSnapshotItem item;
         bool error = false;
         bool ignore = false;
         bool eof = false;
@@ -1054,7 +1054,7 @@ void TestNetworkJobs::testFullFileListWithCursorCsvBlacklist() {
 
     auto counter = 0;
     const std::string cursor = job.getCursor();
-    SnapshotItem item;
+    RemoteSnapshotItem item;
     bool error = false;
     bool ignore = false;
     bool eof = false;
@@ -1080,7 +1080,7 @@ void TestNetworkJobs::testFullFileListWithCursorMissingEof() {
 
     int counter = 0;
     const std::string cursor = job.getCursor();
-    SnapshotItem item;
+    RemoteSnapshotItem item;
     bool error = false;
     bool ignore = false;
     bool eof = false;
