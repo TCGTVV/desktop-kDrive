b49b3c709cccb928b359b0cdd1faae40e41a8e0f
Luc Guyot <luc.guyot@infomaniak.com>
2026-04-10 14:11:40 +0200

    refactor(abstract-token-network-job): Removes user ID from AbsractTokenNetworkJob constructor as it is not used

--- test/libsyncengine/jobs/network/testnetworkjobs.cpp
+++ test/libsyncengine/jobs/network/testnetworkjobs.cpp
@@ -26,6 +26,7 @@
 #include "jobs/network/kDrive_API/getdriveslistjob.h"
 #include "jobs/network/kDrive_API/getfileinfojob.h"
 #include "jobs/network/kDrive_API/getfilelistjob.h"
+#include "jobs/network/kDrive_API/getrootfilelistjob.h"
 #include "jobs/network/infomaniak_API/getinfouserjob.h"
 #include "jobs/network/kDrive_API/getinfodrivejob.h"
 #include "jobs/network/kDrive_API/getthumbnailjob.h"
@@ -943,7 +944,7 @@ void TestNetworkJobs::testGetFileList() {
         }
 
         for (uint16_t page = 1; page <= 2; page++) {
-            GetFileListJob job(_driveDbId, tmpRemoteDir.id(), page, true, 10);
+            GetFileListJob job(_driveDbId, tmpRemoteDir.id(), FileListJobConfig{page, true, 10});
             (void) job.runSynchronously();
             Poco::JSON::Object::Ptr resObj = job.jsonRes();
             CPPUNIT_ASSERT(resObj);
@@ -978,7 +979,7 @@ void TestNetworkJobs::testGetFileListWithCursor() {
 
 void TestNetworkJobs::testFullFileListWithCursorCsv() {
     {
-        CsvFullFileListWithCursorJob job(_driveDbId, "1", {}, CsvFullFileListWithCursorJob::Zip::Off);
+        CsvFullFileListWithCursorJob job(_driveDbId, RemoteNodeId{"1"}, {}, CsvFullFileListWithCursorJob::Zip::Off);
         const ExitInfo exitInfo = job.runSynchronously();
         CPPUNIT_ASSERT_EQUAL(ExitInfo(ExitCode::Ok), exitInfo);
 
@@ -1002,7 +1003,7 @@ void TestNetworkJobs::testFullFileListWithCursorCsv() {
 
 void TestNetworkJobs::testFullFileListWithCursorCsvZip() {
     {
-        CsvFullFileListWithCursorJob job(_driveDbId, "1", {}, CsvFullFileListWithCursorJob::Zip::On);
+        CsvFullFileListWithCursorJob job(_driveDbId, RemoteNodeId{"1"}, {}, CsvFullFileListWithCursorJob::Zip::On);
         const ExitInfo exitInfo = job.runSynchronously();
         CPPUNIT_ASSERT_EQUAL(ExitInfo(ExitCode::Ok), exitInfo);
 
