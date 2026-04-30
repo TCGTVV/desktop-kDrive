58ee498c1ef84ecc4ad6c8ce0491ad3f57881e2c
Luc Guyot <luc.guyot@infomaniak.com>
2026-04-30 08:46:13 +0200

    fix(local-file-system-observer): Prevents the removal of the root item from the local snapshot when an edit event is detected for the main sync folder

--- src/libsyncengine/update_detection/file_system_observer/localfilesystemobserverworker.cpp
+++ src/libsyncengine/update_detection/file_system_observer/localfilesystemobserverworker.cpp
@@ -322,9 +322,10 @@ ExitInfo LocalFileSystemObserverWorker::changesDetected(
                                                                                       << L" (" << CommonUtility::s2ws(itemId)
                                                                                       << L")");
                 } else {
-                    LOGW_SYNCPAL_WARN(_logger, L"Failed to remove item: " << Utility::formatSyncPath(absolutePath) << L" ("
+                    LOGW_SYNCPAL_WARN(_logger, L"Failed to remove item from local snapshot: " << Utility::formatSyncPath(absolutePath) << L" ("
                                                                           << CommonUtility::s2ws(itemId) << L")");
                     invalidateSnapshot();
+
                     return ExitCode::DataError;
                 }
                 continue;
@@ -332,17 +333,14 @@ ExitInfo LocalFileSystemObserverWorker::changesDetected(
 
             NodeId previousItemId;
             if (const auto exitInfo = _liveSnapshot.getItemId(relativePath, previousItemId); exitInfo) {
-                // If an item with the same path already exists, remove it from snapshot because its ID might have changed (i.e.
-                // the file has been downloaded in the tmp folder then moved to override the existing one). The item will be
-                // inserted below anyway.
-                if (!previousItemId.empty() && _liveSnapshot.removeItem(previousItemId)) {
-                    LOGW_SYNCPAL_DEBUG(_logger, L"Item removed from local snapshot: "
-                                                        << Utility::formatSyncPath(absolutePath) << L" ("
-                                                        << CommonUtility::s2ws(previousItemId) << L")");
+
+            if (_liveSnapshot.pathExists(relativePath)) {
+
                 } else {
                     LOGW_SYNCPAL_WARN(_logger, L"Failed to delete item: " << Utility::formatSyncPath(absolutePath) << L" ("
                                                                           << CommonUtility::s2ws(previousItemId) << L")");
                     invalidateSnapshot();
+
                     return ExitCode::DataError;
                 }
             } else {
