dc71604ef63bfd42c42d1daa01eac75b3dff835e
Luc Guyot <luc.guyot@infomaniak.com>
2026-05-21 11:54:59 +0200

    fix(unit-tests): Fixes unit tests build failures caused by incorrect merge conflict resolution

--- test/libsyncengine/syncpal/testsyncpalworker.cpp
+++ test/libsyncengine/syncpal/testsyncpalworker.cpp
@@ -380,6 +380,16 @@ void TestSyncPalWorker::MockSyncPal::createWorkers(const std::chrono::seconds &s
     _tmpBlacklistManager = std::make_shared<TmpBlacklistManager>(shared_from_this());
 }
 
+void TestSyncPalWorker::MockSyncPal::freeSnapshotsCopies() {
+    // Ensure that no shared_ptr outside of SyncPal holds a reference to the snapshots to avoid them being kept alive while the
+    // workers are being destroyed, which would cause use-after-free when the workers try to access them during their destruction.
+    assert(_localSnapshot.use_count() <= 1);
+    _localSnapshot.reset();
+
+    assert(_remoteSnapshot.use_count() <= 1);
+    _remoteSnapshot.reset();
+}
+
 ExitInfo TestSyncPalWorker::MockRemoteFileSystemObserverWorker::checkIfRemoteDirHasChanges(const RemoteNodeId &, const bool,
                                                                                            const LongPollJobMap &,
                                                                                            bool &hasChanges) {
@@ -391,7 +401,7 @@ ExitInfo TestSyncPalWorker::MockRemoteFileSystemObserverWorker::checkIfRemoteDir
 ExitInfo TestSyncPalWorker::MockRemoteFileSystemObserverWorker::updateLongPollJobs(
         const std::vector<RemoteNodeId> &, RemoteFileSystemObserverWorker::LongPollJobMap &) {
     if (!_networkAvailable) return ExitCode::NetworkError;
-    
+
     return ExitCode::Ok;
 }
 
