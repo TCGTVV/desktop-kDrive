95ebf93fc3964f6983d91c6c112f26e117abc72e
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-05-21 15:31:18 +0200

    chore: Improve header

--- src/gui4/macOS/kDriveCore/Models/DTOs/Synchro.swift
+++ src/gui4/macOS/kDriveCore/Models/DTOs/Synchro.swift
@@ -138,7 +138,7 @@ extension SynchroNode {
 public enum SynchroError: Error, Hashable, Sendable, CaseIterable {
     case asleep
     case wakingUp
-    case notRenew // "drive locked"
+    case notRenew
     case maintenance
     case accessDenied
     case loggingError
